3e98b05289a00fe020f06a353505d5481096b52e
salah-abukhatwa <roben.gn2@gmail.com>
2024-11-20 14:53:48 +0300

    Add buttom &  control component and enhance style

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -6,7 +6,24 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
+  // host: {
+  //   class: 'status',
+  // },
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  currentStatus: 'online' | 'offline' | 'unKnown' = 'online';
+
+  constructor() {
+    setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd > 0.5) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unKnown';
+      }
+    }, 5000);
+  }
 }
