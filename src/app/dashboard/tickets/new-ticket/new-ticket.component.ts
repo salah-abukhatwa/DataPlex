ebd792112a729975ab2b5362951b5dfb4d5bb16f
salah-abukhatwa <roben.gn2@gmail.com>
2024-11-24 20:26:07 +0300

    Forms submisstions and Add ticket component

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, output } from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  add = output<{ title: string; text: string }>();
+  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
+    this.add.emit({ title: title, text: ticketText });
+    form.reset;
+  }
+}
--- src/app/dashboard/tickets/ticket/ticket.component.ts
+++ src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
   standalone: true,
   imports: [],
   templateUrl: './ticket.component.html',
-  styleUrl: './ticket.component.css'
+  styleUrl: './ticket.component.css',
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  detailsVisible = signal(false);
+  close = output();
 
+  onToggleDetails() {
+    this.detailsVisible.set(!this.detailsVisible());
+  }
+
+  onMarkCompleted() {
+    this.close.emit();
+  }
 }
